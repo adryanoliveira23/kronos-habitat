6086a1138f38fd8e786f66302d77fdf8588949f1
Adryan Oliveira <blessedt402@gmail.com>
2026-03-08 12:16:02 -0300

    update

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -3,11 +3,14 @@ import * as admin from "firebase-admin";
 const firebaseAdminConfig = {
   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY
-    ? process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n")
-        .replace(/^"(.*)"$/, "$1")
-        .trim()
-    : undefined,
+  privateKey: (() => {
+    let key = process.env.FIREBASE_ADMIN_PRIVATE_KEY;
+    if (!key) return undefined;
+    key = key.replace(/\\n/g, "\n");
+    if (key.startsWith('"')) key = key.substring(1);
+    if (key.endsWith('"')) key = key.substring(0, key.length - 1);
+    return key.trim();
+  })(),
 };
 
 if (!admin.apps.length) {
